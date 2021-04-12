f7f1539b756fcc40028a65228f0b43dd7a6a9084
Mouse Reeve <mousereeve@riseup.net>
2021-04-12 16:18:01 -0700

    copies documentation over from bookwyrm.social

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Mouse Reeve'
+SITENAME = 'BookWyrm Documentation'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'US/Pacific'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('GitHub', 'https://github.com/mouse-reeve/bookwyrm'),
+        ('Patreon', 'https://www.patreon.com/bookwyrm'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+PORT = 7777
