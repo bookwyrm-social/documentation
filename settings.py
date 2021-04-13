bc50051fcf38fc539b3593f212e190b92d0b9d0c
Mouse Reeve <mousereeve@riseup.net>
2021-04-13 11:59:41 -0700

    Fancier donate button

--- settings.py
+++ settings.py
@@ -20,8 +20,8 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-LINKS = (('Code', 'https://github.com/mouse-reeve/bookwyrm'),
-        ('Donate', 'https://www.patreon.com/bookwyrm'),)
+LINKS = (('Code', 'https://github.com/mouse-reeve/bookwyrm', ''),
+        ('♥ Donate', 'https://www.patreon.com/bookwyrm', 'button'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
