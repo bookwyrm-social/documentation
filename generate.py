6e2d488bdb426d89567afbe78d05569ad6469b36
Hugh Rundle <hugh@hughrundle.net>
2026-03-20 19:11:38 +1100

    add v0.8.5

--- generate.py
+++ generate.py
@@ -175,7 +175,7 @@ if __name__ == "__main__":
                         if not version
                         else f"/{version}/{SLUG}{output_path}"
                     )
-                    versions = ["development", "v0.7.5", "v0.8.0", "v0.8.2"]
+                    versions = ["development", "v0.7.5", "v0.8.0", "v0.8.2", "v0.8.5"]
                     current_version = version if version else ""
                     render_file.write(
                         template.render(
